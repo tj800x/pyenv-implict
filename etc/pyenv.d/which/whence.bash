6343cc378d7a71de7cbce5029f01371a05099441
Tom Johnson <tj800x@gmail.com>
2019-08-06 09:06:19 -0400

    Fixed implicit lookup to default to most specific local version of command.

--- etc/pyenv.d/which/whence.bash
+++ etc/pyenv.d/which/whence.bash
@@ -1,9 +1,15 @@
 if [ -n "$PYENV_COMMAND" ] && [ ! -x "$PYENV_COMMAND_PATH" ]; then
   versions=($(pyenv-whence "${PYENV_COMMAND}" 2>/dev/null || true))
+  localversions=($(pyenv-local | sort -r))
   if [ -n "${versions}" ]; then
     if [ "${#versions[@]}" -gt 1 ]; then
-      echo "pyenv-implicit: found multiple ${PYENV_COMMAND} in pyenv. Use version ${versions[0]}." 1>&2
+      if [ "${#localversions[@]}" -gt 1 ]; then
+        echo "pyenv-implicit: found multiple ${PYENV_COMMAND} in pyenv. Using latest local version ${localversions[0]}." 1>&2
+        PYENV_COMMAND_PATH="${PYENV_ROOT}/versions/${localversions[0]}/bin/${PYENV_COMMAND}"
+      else
+        echo "pyenv-implicit: found multiple ${PYENV_COMMAND} in pyenv. Using version ${versions[0]}." 1>&2
+        PYENV_COMMAND_PATH="${PYENV_ROOT}/versions/${versions[0]}/bin/${PYENV_COMMAND}"
+      fi
     fi
-    PYENV_COMMAND_PATH="${PYENV_ROOT}/versions/${versions[0]}/bin/${PYENV_COMMAND}"
   fi
 fi
